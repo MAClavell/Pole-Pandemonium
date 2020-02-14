174ef8c7e8393a336622fc258dfbf8029b760259
pwoodsum <pwoodsum@autonodyne.com>
2020-02-14 17:21:09 -0500

    Pole falls based on Gravite constant set in the GameManager script. Add force function on the Pole script can be called to add an instant tap force.

---
+++ Z-Team Game 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // The force due to gravity
+    public static readonly float GRAVITY = 9.8f;
+
+    public Pole pole;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        pole.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pole.Update();
+    }
+}
--- Z-Team Game 2/Assets/Scripts/Pole.cs
+++ Z-Team Game 2/Assets/Scripts/Pole.cs
@@ -4,38 +4,113 @@ using UnityEngine;
 
 public class Pole : MonoBehaviour
 {
-    float accelaration;
-    float velocity;
-    float mass;
-    float rot;
-    Quaternion previousRotation;
-    Vector3 angularVelocity;
+    public float currentRotation { get; private set; }
+
+    private float totalForce;
+
+    private float resistance;
+
+    private float mass;
+
+    private float height;
 
     void Awake()
     {
-        accelaration = -1000;
-        velocity = 0;
-        mass = 1;
-        rot = 0;
+        resistance = 1.0f;
+
+        mass = 1.0f;
+
+        height = gameObject.GetComponent<SpriteRenderer>().sprite.rect.height;
+
+        Debug.Log("Pole Height: " + height);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-    }
-
-    void FixedUpdate()
-    {
 
     }
 
-    private void Update()
-    {
 
+    public void Init()
+    {
+        // Add an initial force to make the pole fall for testing purposes
+        AddForce(10.0f);
     }
 
-    void AddForce(float force)
+    public void Update()
     {
-        accelaration += force / mass;
+        RotatePole();
+        totalForce = 0.0f;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="force">The amount of the force that is being applied. Negative is counter clockwise, Positive is clockwise</param>
+    /// <param name="vPos">At what height on the pole is it being applied to</param>
+    public void AddForce(float force, float vPos = 1.0f)
+    {
+        totalForce += force;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="position">The position of the collision</param>
+    /// <param name="mass">The mass of the colliding object</param>
+    /// <param name="velocity">The Velocity of the colliding object</param>
+    /// <param name="attach">Does the colliding object stick to the pole?</param>
+    public void Collide(Vector2 position, float mass, Vector2 velocity, bool attach = false)
+    {
+        // Assuming that if the x velocity is position, then the pole should be pushed clockwise and vice versa 
+        int direction = velocity.x < 0 ? -1 : 1;
+
+        // Only care about the force that is perpendicular to the pole.
+        float force = direction * 5.0f;
+
+        // Find where along the pole the collision happend based on the position of the collision
+        float vPos = 0.5f;
+
+        AddForce(force, vPos);
+
+        if (attach)
+        {
+            AddMass(mass);
+        }
+    }
+
+    /// <summary>
+    /// Add a mass to the pole
+    /// </summary>
+    /// <param name="mass">The amount of mass to add</param>
+    /// <param name="vPos">How high up the pole the mass is </param>
+    /// <param name="offSet">The distance offset perpendicular to the pole the mass is applied</param>
+    /// <param name="side">The direction of the offset. Negative is left side, positive is right side</param>
+    public void AddMass(float mass, float vPos = 0.5f, float offSet = 0.0f, int side = 1)
+    {
+        
+    }
+
+    /// <summary>
+    /// Calculate the force due to gravity then rotate the pole based on all of the acting forces.
+    /// </summary>
+    private void RotatePole()
+    {
+        int direction = transform.eulerAngles.z < 0 ? -1 : 1;
+
+        float gravForce = direction * GameManager.GRAVITY * resistance * (Mathf.Sin((Mathf.Abs(transform.eulerAngles.z) * Mathf.Deg2Rad)));
+
+        //Debug.Log(gravForce);
+
+        totalForce += gravForce;
+
+        float rotation = Mathf.Pow((totalForce / mass), 2) * Time.deltaTime;
+
+        currentRotation += rotation;
+
+        currentRotation = Mathf.Clamp(currentRotation, -90.0f, 90.0f);
+
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRotation);
     }
 }
