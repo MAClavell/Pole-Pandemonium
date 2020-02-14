fe61a58957bd8885455e0239aa8cab88fa68c3a5
Michael Clavell (RIT Student) <mac9406@ad.rit.edu>
2020-02-14 10:51:16 -0500

    Testing physics

---
+++ Z-Team Game 2/Assets/Scripts/Pole.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pole : MonoBehaviour
+{
+    float accelaration;
+    float velocity;
+    float mass;
+    float rot;
+    Quaternion previousRotation;
+    Vector3 angularVelocity;
+
+    void Awake()
+    {
+        accelaration = -1000;
+        velocity = 0;
+        mass = 1;
+        rot = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void AddForce(float force)
+    {
+        accelaration += force / mass;
+    }
+}
